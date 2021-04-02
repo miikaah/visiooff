9198a798f8115ea3eae7883383953eb09bd82b7d
Miika Henttonen <miika.henttonen@gmail.com>
2021-04-02 11:43:58 +0300

    Remove content in interval of 1 sec because hs.fi loads content lazily

--- visiooff.js
+++ visiooff.js
@@ -1,10 +1,16 @@
-console.log("Removing Visio content");
+console.log("Activating Visio content remover");
 
-var allAnchors = document.querySelectorAll("a");
-var visioContent = Array.from(allAnchors).filter((a) =>
-  (a.href || "").includes("/visio")
-);
+function removeVisioContent() {
+  var allAnchors = document.querySelectorAll("a");
+  var visioContent = Array.from(allAnchors).filter((a) =>
+    (a.href || "").includes("/visio")
+  );
 
-visioContent.forEach((v) => (v.parentElement || {}).remove());
+  visioContent.forEach((v) => (v.parentElement || {}).remove());
 
-console.log(`Removed ${visioContent.length} Visio contents`);
+  if (visioContent.length > 0) {
+    console.log(`Removed ${visioContent.length} Visio contents`);
+  }
+}
+
+setInterval(removeVisioContent, 1000);
